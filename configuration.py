9d72826a96f9b4672b3d71686aacd21d9cd52e70
DariPtitsyna <sumpatheia@gmail.com>
2024-06-20 19:37:32 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://15a62ddc-1b68-476b-adcd-8239467d53c8.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+NEW_ORDER = "/api/v1/orders"
+TRACK_ORDER = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,21 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "JopenPopen",
+    "lastName": "Jopkovich",
+    "address": "Bananovaya, 15",
+    "metroStation": 1,
+    "phone": "+7 800 666 66 66",
+    "rentTime": 1,
+    "deliveryDate": "2024-20-06",
+    "comment": "None",
+    "color": [
+        "BLACK"
+    ]
+}
+
+params_get = {
+    "t": ""
+}
